7dd4e6f293635cbce42614d845abb5f6fb5d8db6
denghongyu <1848761082@qq.com>
2022-11-12 17:36:50 +0800

    fix：更换打包方式

--- vite.config.js
+++ vite.config.js
@@ -39,12 +39,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    minify: 'esbuild'
   }
 })
