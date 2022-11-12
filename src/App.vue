b964ff18adb8a7481383b2dd0baeb2cc10d63e7c
denghongyu <1848761082@qq.com>
2022-11-12 17:18:35 +0800

    fix: 删除切换效果

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <router-view v-slot="{ Component }">
-    <transition name="router_animate">
-      <keep-alive :include="store.getters.keepalive">
-        <component
-          :is="Component"
-          :key="$route.name"
-        />
-      </keep-alive>
-    </transition>
+    <keep-alive :include="store.getters.keepalive">
+      <component
+        :is="Component"
+        :key="$route.name"
+      />
+    </keep-alive>
   </router-view>
   <MyLoading :show="store.getters.showLoading" />
 </template>
@@ -15,12 +13,3 @@
 import { useStore } from 'vuex'
 const store = useStore()
 </script>
-
-<style scoped>
-.router_animate-enter-active {
-  animation: slideInLeft 0.2s;
-}
-.router_animate-leave-active {
-  animation: slideOutLeft 0.2s;
-}
-</style>
